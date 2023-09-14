7a30eb785cfe35c55b7d1a27557c3263581eb407
Seb0927 <sebastianidrobo@hotmail.com>
2023-09-14 18:14:23 -0500

    "Images" code added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -24,7 +24,7 @@ const Book = () => {
 }
 
 const Image = () => (
-  <img src='https://images-na.ssl-images-amazon.com/images/I/814mI0-rkxL._AC_UL600_SR600,400_.jpg' alt='Elon Musk' />
+  <img src='./images/book-1.jpg' alt='Elon Musk' />
 );
 const Title = () => <h2>Elon Musk</h2>
 const Author = () => {
