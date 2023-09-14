66b541fd111af0512d26d09128c06fa8e60f9a4b
Seb0927 <sebastianidrobo@hotmail.com>
2023-09-14 18:41:39 -0500

    "JSX - CSS" code added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -28,8 +28,13 @@ const Image = () => (
 );
 const Title = () => <h2>Elon Musk</h2>
 const Author = () => {
-  return <h4>Walter Isaacson</h4>
-}
+  const inlineHeadingStyles = {
+    color: '#617d98',
+    fontSize: '0.75rem',
+    marginTop: '0.5rem',
+  };
+  return <h4 style={inlineHeadingStyles}>Jordan Moore </h4>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
