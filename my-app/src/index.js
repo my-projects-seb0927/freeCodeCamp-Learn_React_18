b10369a9a013f4175805f12cb7e7d705e1f9c889
Seb0927 <sebastianidrobo@hotmail.com>
2023-09-14 23:20:46 -0500

    "Access Props - Multiple Approaches" code added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css' 
-const title = 'Elon Musk';
-const author = 'Walter Isaacson'
-const img = './images/book-1.jpg'
+const firstBook = {
+  author: 'Walter Isaacson',
+  title: 'Elon Musk',
+  img: './images/book-1.jpg'
+}
+
+const secondBook = {
+  author: ' Arthur C. Brooks',
+  title: 'Build the Life You Want:',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81+MJor-K6L._AC_UL600_SR600,400_.jpg'
+}
 
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book author={author} title={title} img={img} />
-      <Book author={author} title={title} img={img} />
+      <Book author={firstBook.author} title={firstBook.title} img={firstBook.img} />
+      <Book author={secondBook.author} title={secondBook.title} img={secondBook.img} />
     </section>
   );
 }
 
 const Book = (props) => {
-  console.log(props);
+  const { img, title, author } = props;
   return (
     <article className='book'>
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author} </h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author} </h4>
     </article>
   );
 };
