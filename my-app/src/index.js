29d12d99b4410f6ec507875c49c1e27213c37178
Seb0927 <sebastianidrobo@hotmail.com>
2023-09-19 23:19:55 -0500

    "Simple List" code added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,45 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css' 
-const firstBook = {
-  author: 'Walter Isaacson',
-  title: 'Elon Musk',
-  img: './images/book-1.jpg'
-}
+const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: './images/book-1.jpg',
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+  },
+];
 
-const secondBook = {
-  author: ' Arthur C. Brooks',
-  title: 'Build the Life You Want:',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81+MJor-K6L._AC_UL600_SR600,400_.jpg'
-}
+const names = ['john', 'peter', 'susan'];
+const newNames = names.map((name) => {
+  console.log(name);
+  return <h1>{name}</h1>;
+});
 
-const BookList = () => {
-  return (
-    <section className='booklist'>
-      <Book
-      author={firstBook.author}
-      title={firstBook.title}
-      img={firstBook.img}
-  >
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
-      repudiandae inventore eos qui animi sed iusto alias eius ea sapiente.
-    </p>
-    <button>click me</button>
-  </Book>
-  <Book author={secondBook.author} title={secondBook.title} img={secondBook.img} />
-    </section>
-  );
+function BookList() {
+  return <section className='booklist'>{newNames}</section>;
 }
 
 const Book = (props) => {
-  const { img, title, author, children } = props;
+  const { img, title, author } = props;
+
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author} </h4>
-      {children}
     </article>
   );
 };
