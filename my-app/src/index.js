542f299347447d7a10a713f2433400e8fa37ab11
Seb0927 <sebastianidrobo@hotmail.com>
2023-09-21 00:18:20 -0500

    "ES6 Modules" code added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css' 
-const books = [
-  {
-    author: 'Jordan Moore',
-    title: 'Interesting Facts For Curious Minds',
-    img: './images/book-1.jpg',
-    id:1,
-  },
-  {
-    author: 'James Clear',
-    title: 'Atomic Habits',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
-    id:2,
-  },
-];
+import {books} from './books';
+import Book from './Book'
 
 const BookList = () => {
   const getBook = (id) => {
@@ -28,22 +16,6 @@ const BookList = () => {
         return <Book {...book} key={book.id} getBook={getBook} />;
       })}
     </section>
-  );
-};
-
-const Book = (props) => {
-  const { img, title, author, getBook, id } = props;
-  // console.log(props);
-  const getSingleBook = () => {
-    getBook(id);
-  };
-  return (
-    <article className='book'>
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={getSingleBook}>display title</button>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
